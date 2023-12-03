f97faecb9fb4662d0959a417efe8877ec07314a3
xuf <xuf_001@126.com>
2023-12-03 19:18:13 +0800

    66. Plus One

--- src/main/java/Array/Easy1To10.java
+++ src/main/java/Array/Easy1To10.java
@@ -2,13 +2,36 @@ package Array;
 
 public class Easy1To10 {
     public static void main(String[] args) {
-        int tes1 = 1+(3-1)>>1;
+        //66. Plus One
+        int[] orig = {1,2,3};
+        //orig = new int[orig.length+1];
+        System.out.println(orig);
+        plusOne(orig);
+
+        //35. Search Insert Position
+        /*int tes1 = 1+(3-1)>>1;
         int tes2 = 1+((3-1)>>1);
         System.out.println(tes1+","+tes2);
         int[] test1 = {1,3};
-        System.out.print(searchInsert(test1,2));
+        System.out.print(searchInsert(test1,2));*/
     }
 
+    //66. Plus One
+    public static int[] plusOne(int[] digits) {
+        if (digits==null){
+            return digits;
+        }
+        for(int i=digits.length-1;i>=0;i--){
+            if(digits[i] < 9){
+                digits[i]++;
+            }
+            return digits;
+        }
+        digits = new int[digits.length+1];
+        digits[0]=1;
+        return digits;
+    }
+    //35. Search Insert Position
     public static int searchInsert(int[] nums, int target) {
         int start = 0;
         int end = nums.length-1;
