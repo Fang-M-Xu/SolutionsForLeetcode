dbafa6f5d57488a9b2b3437a4b662f4d756cb529
Fangx <fang.xs@outlook.com>
2023-12-29 13:42:07 +0800

    704. Binary Search

--- src/main/java/Array/Easy20T40.java
+++ src/main/java/Array/Easy20T40.java
@@ -6,13 +6,50 @@ import java.util.Map;
 public class Easy20T40 {
 
     public static void main(String[] args) {
+
+        //704. Binary Search
+        int[] param1 = {1,3,6,8,16,20};
+        int result = search(param1,18);
         //724. Find Pivot Index
-        int[] param1 = {1,7,3,6,5,6};
-        int result = pivotIndex(param1);
-        //697. Degree of an Array
-        //int[] param1 = {1,2,2,3,1,4,2};
+        //int result = pivotIndex(param1);
+        //697. Degree of an Array{1,2,2,3,1,4,2};
         //int result = findShortestSubArray(param1);
         System.out.println(result);
+    }
+    //724. Find Pivot Index
+    public static int pivotIndex(int[] nums) {
+        int left_sum = 0;
+        int right_sum = 0;
+        for(int num:nums){
+            right_sum += num;
+        }
+
+        for(int i  = 0; i< nums.length;i++){
+            right_sum -= nums[i];
+            if(left_sum == right_sum){
+                return i;
+            }
+            left_sum += nums[i];
+        }
+        return -1;
+    }
+
+    //704.Binary Search
+    public static int search(int[] nums, int target) {
+        int left_index = 0;
+        int right_index = nums.length - 1;
+
+        while(left_index<=right_index){
+            int middle_index = left_index+(right_index-left_index)/2;
+            if(target == nums[middle_index]){
+                return middle_index;
+            }else if(target > nums[middle_index]){
+                left_index = middle_index+1;
+            }else if(target < nums[middle_index]){
+                right_index = middle_index-1;
+            }
+        }
+        return -1;
     }
 
     //697. Degree of an Array
@@ -43,23 +80,5 @@ public class Easy20T40 {
             }
         }
         return min_len;
-    }
-
-    //724. Find Pivot Index
-    public static int pivotIndex(int[] nums) {
-        int left_sum = 0;
-        int right_sum = 0;
-        for(int num:nums){
-            right_sum += num;
-        }
-
-        for(int i  = 0; i< nums.length;i++){
-            right_sum -= nums[i];
-            if(left_sum == right_sum){
-                return i;
-            }
-            left_sum += nums[i];
-        }
-        return -1;
     }
 }
