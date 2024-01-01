125b3259fe42dd788babd68496556b29fe36f797
Fangx <fang.xs@outlook.com>
2024-01-01 17:14:07 +0800

    15. 3Sum

--- src/main/java/Array/Medium10T60.java
+++ src/main/java/Array/Medium10T60.java
@@ -1,8 +1,34 @@
 package Array;
 
+import java.util.*;
+
 public class Medium10T60 {
     public static void main(String[] args) {
 
+    }
+
+    //15. 3Sum
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        Set<List<Integer>> triplets = new HashSet<>();
+        for(int i=0;i<nums.length-2; i++){
+            int j = i+1;
+            int k = nums.length-1;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum==0){
+                    triplets.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    j++;
+                    k--;
+                }else if(sum<0){
+                    j++;
+                }else if(sum>0){
+                    k--;
+                }
+            }
+        }
+        List<List<Integer>> result = new ArrayList<>(triplets);
+        return result;
     }
 
     //11. Container With Most Water
