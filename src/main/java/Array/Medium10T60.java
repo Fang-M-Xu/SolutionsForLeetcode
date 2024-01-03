a183e82780a57f0076a8fbc59a785c3c9ca3bba6
Fangx <fang.xs@outlook.com>
2024-01-03 11:19:10 +0800

    33. Search in Rotated Sorted Array

--- src/main/java/Array/Medium10T60.java
+++ src/main/java/Array/Medium10T60.java
@@ -5,14 +5,41 @@ import java.util.*;
 
 public class Medium10T60 {
     public static void main(String[] args) {
+
+        int[] param1={5,1,3};
+        //33. Search in Rotated Sorted Array
+        int res = search(param1,3);
         //31. Next Permutation
-        int[] param1={1,2,3};
-        nextPermutation(param1);
+        //nextPermutation(param1);
         //18. 4Sum
         //int[] param1={1,0,-1,0,-2,2};
         //fourSum(param1,0);
-        System.out.println("Good!");
+        System.out.println(res);
     }
+    //33. Search in Rotated Sorted Array
+    public static int search(int[] nums, int target) {
+        int left=0;
+        int right = nums.length-1;
+        while(left<=right){
+            int middle = left+(right-left)/2;
+            if(nums[middle]==target) return middle;
+            if(nums[left]<=nums[middle]){
+                if(nums[left]<=target && target<nums[middle]){
+                    right=middle-1;
+                }else{
+                    left=middle+1;
+                }
+            }else{
+                if(nums[middle]<target && target<=nums[right]){
+                    left=middle+1;
+                }else{
+                    right=middle-1;
+                }
+            }
+        }
+        return -1;
+    }
+
 
     //31. Next Permutation
     public static void nextPermutation(int[] nums) {
--- src/main/java/Tets/TestForArray.java
+++ src/main/java/Tets/TestForArray.java
@@ -2,8 +2,11 @@ package Tets;
 
 public class TestForArray {
     public static void main(String[] args) {
-        int res = -294967296-1000000000;
+        int tem= 4+(5-4)/2;
+        System.out.println(tem);
+        /*int res = -294967296-1000000000;
         res = res - 1000000000;
         System.out.println(res);
+        */
     }
 }
