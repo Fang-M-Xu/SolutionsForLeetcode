b3ed92128869cd27a64a83c410940cbec1a9f924
xuf <xuf_001@126.com>
2024-01-07 11:42:17 +0800

    54. Spiral Matrix-judge the edge position

--- src/main/java/Array/Medium10T60.java
+++ src/main/java/Array/Medium10T60.java
@@ -32,6 +32,39 @@ public class Medium10T60 {
         //fourSum(param1,0);
         System.out.println("res");
     }
+    //54. Spiral Matrix-judge the edge position
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        int top =0,bottom=matrix.length-1, left=0, right=matrix[0].length-1;
+        while(top<=bottom&&left<=right){
+            if(top<=bottom){
+                for(int i=left; i<=right; i++){
+                    result.add(matrix[top][i]);
+                }
+                top++;
+            }
+            if(left<=right){
+                for(int i=top; i<=bottom; i++){
+                    result.add(matrix[i][right]);
+                }
+                right--;
+            }
+            if(top<=bottom){
+                for(int i=right; i>=left;i--){
+                    result.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if(left<=right){
+                for(int i=bottom; i>=top; i--){
+                    result.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return result;
+    }
+
     //49. Group Anagrams
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String,List<String>> group = new HashMap<>();
