b4970dfd57a8f25a3ff6ee98885bfb11a103ca18
xuf <xuf_001@126.com>
2024-01-08 13:22:21 +0800

    73. Set Matrix Zeroes

--- src/main/java/Array/Medium60T200.java
+++ src/main/java/Array/Medium60T200.java
@@ -7,7 +7,44 @@ public class Medium60T200 {
         int[][] duparam = {{1,2,3}, {4,5,6}};
         minPathSum(duparam);
     }
+    //73. Set Matrix Zeroes
+    public void setZeroes(int[][] matrix) {
+        boolean row0=false, col0=false;
+        int m=matrix.length,n=matrix[0].length;
+        for(int i=0;i<n;i++){
+            if(matrix[0][i]==0) row0=true;
+        }
+        for(int i=0;i<m;i++){
+            if(matrix[i][0]==0) col0=true;
+        }
 
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]==0){
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+                }
+            }
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[0][j]==0||matrix[i][0]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if(row0==true){
+            for(int i=0;i<n;i++){
+                matrix[0][i]=0;
+            }
+        }
+        if(col0==true){
+            for(int i=0;i<m;i++){
+                matrix[i][0]=0;
+            }
+        }
+    }
     //64. Minimum Path Sum
     public static int minPathSum(int[][] grid) {
         int[][] path_grid = new int[grid.length][grid[0].length];
